c57af4ca94a2c5e379aa9985ad8dcb76c22b0fc2
admin <1904474691@qq.com>
2025-05-16 14:04:22 +0800

    实现后端限流操作和前端深色模式的适配

---
+++ my-project-backend/src/main/java/com/example/filter/FLowLimitFilter.java
@@ -0,0 +1,60 @@
+package com.example.filter;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Synchronized;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import com.example.utils.Const;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Order(Const.ORDER_LIMIT)
+public class FLowLimitFilter extends HttpFilter {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String ip = request.getRemoteAddr();
+        if(this.trycount(ip)){
+            chain.doFilter(request, response);
+        }else{
+            this.writeBlcokMessage(response);
+        }
+    }
+    private void writeBlcokMessage(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write("操作频繁请稍后再试");
+    }
+    private boolean trycount(String ip){
+        synchronized (ip.intern()) {
+            if(Boolean.TRUE.equals(stringRedisTemplate.hasKey(Const.FLOW_LIMIT_BLOCK+ip)))
+                return false;
+            return this.limitPeriodBlock(ip);
+        }
+
+
+    }
+    private boolean limitPeriodBlock(String ip){
+        if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(Const.FLOW_LIMIT_COUNTER+ip))) {
+            Long increment= Optional.ofNullable(stringRedisTemplate.opsForValue().increment(Const.FLOW_LIMIT_COUNTER+ip)).orElse(0L);
+            if(increment>20){
+                stringRedisTemplate.opsForValue().set(Const.FLOW_LIMIT_BLOCK+ip,"",30, TimeUnit.SECONDS);
+            }
+
+        }else{
+            stringRedisTemplate.opsForValue().set(Const.FLOW_LIMIT_COUNTER+ip,"1",3, TimeUnit.SECONDS);
+        }
+     return true;
+    }
+}
--- my-project-backend/src/main/java/com/example/utils/Const.java
+++ my-project-backend/src/main/java/com/example/utils/Const.java
@@ -3,6 +3,9 @@ package com.example.utils;
 public class Const {
     public static final String JWT_BLACK_LIST="jwt:blacklist";
     public static final int ORDER_CORS=-102;
+    public static final int ORDER_LIMIT=-101;
     public final static String VERIFY_EMAIL_LIMIT = "verify:email:limit:";
     public final static String VERIFY_EMAIL_DATA = "verify:email:data:";
+    public final static String FLOW_LIMIT_COUNTER = "flow:counter:";
+    public final static String FLOW_LIMIT_BLOCK = "flow:block:";
 }
