dffd00f77c5d3b7c0e508016a6c4eb091170eb7e
yangyu <1904474691@qq.com>
2026-02-21 15:20:00 +0800

    feat: 引入MaxKB作为RAG知识库

--- my-project-backend/src/main/java/com/example/service/impl/AIServiceImpl.java
+++ my-project-backend/src/main/java/com/example/service/impl/AIServiceImpl.java
@@ -23,7 +23,7 @@ public class AIServiceImpl implements AIService {
     ChatModel model;
     @Override
     public SseEmitter chatWithAI(JSONArray context) {
-        SseEmitter emitter = new SseEmitter(3000L);
+        SseEmitter emitter = new SseEmitter(5000L);
         List<? extends AbstractMessage> list = context.stream().map(item -> {
             JSONObject obj = JSONObject.from(item);
             return switch (obj.getString("type")) {
